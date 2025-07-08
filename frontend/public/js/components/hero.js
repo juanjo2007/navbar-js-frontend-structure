b457417b87b7f6c215dee025c474ba7ff10ff14e
Juan Jose Ocampo <jujocampore154@gmail.com>
2025-07-07 19:04:14 -0500

    Feat: se agregó el hero, se mejorarón los estilos del navbar

---
+++ frontend/public/js/components/hero.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const heroElement = document.querySelector(".hero-container");
+
+    if(heroElement){
+        fetch("/frontend/public/views/components/hero.html")
+        .then(response => response.text())
+        .then(data => {
+            heroElement.innerHTML = data;
+        })
+    
+
+    .catch(error => console.log("Error cargando el hero", error));
+    }
+});
